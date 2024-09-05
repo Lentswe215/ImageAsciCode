fe144dbd4c7d3f2dc2c446a40cb8e7193e792215
Lentswe215 <ephraimmamonong@gmail.com>
2024-09-05 22:34:51 +0200

    Update the ascii text to show video contents

--- js/script.js
+++ js/script.js
@@ -1,32 +1,31 @@
-const density = "Ñ@#W$9876543210?!abc;:+=-,._                         ";
-
+const density = "Ñ@#W$9876543210?!abc;:+=-,._       ";
+// const density =' .:-i|=+%0#@';
 let video;
 let asciiDiv;
+let cnt = 0;
 
 function setup() {
   noCanvas();
   video = createCapture(VIDEO);
   video.size(64, 48);
-  asciiDiv = createDiv();
+  asciiDiv = createDiv('ascii-block');
+  asciiDiv.position(250, 50)
 }
 
 function draw() {
   video.loadPixels();
   let asciiImage = "";
-  for (var i = 0; i < video.height; i++) {
-    for (var j = 0; j < video.width; j++) {
+  for (var j= 0; j < video.height; j++) {
+    for (var i = 0; i < video.width; i++) {
       const pixelIndex = (i + j * video.width) * 4;
       const r = video.pixels[pixelIndex + 0];
       const g = video.pixels[pixelIndex + 1];
       const b = video.pixels[pixelIndex + 2];
 
-      const avg = (r + g + b) / 3;
-      const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, len, 0));
-
-      let value = density.charAt(charIndex);
-      if (value == "") value = "&nbsp;";
-      asciiImage += value;
+        const avg = (r + g + b) / 3;
+        const len = density.length;
+        const charIndex = floor(map(avg, 0, 255, len, 0));
+        asciiImage += density.charAt(charIndex);;
     }
     asciiImage += "<br/>";
   }
